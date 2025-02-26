dbcc984e2fe9b865307d51de33928c811618bb27
L CODE <leonardo.developer.figueiredo@gmail.com>
2025-02-26 01:18:26 -0300

    Integração API -- emailjs

---
+++ script/newsletter.js
@@ -0,0 +1,26 @@
+function sendNewsletter(e) {
+  e.preventDefault();
+
+  const email = document.getElementById("newsletter-email").value;
+
+  // Send email using EmailJS
+  emailjs
+    .send(
+      "service_at26rze", // Add your EmailJS service ID
+      "template_fhojz9z", // Add your EmailJS template ID
+      {
+        to_email: email,
+        // Add any other template parameters you want to send
+      }
+    )
+    .then(
+      function (response) {
+        alert("Inscrição realizada com sucesso!");
+        document.getElementById("newsletter-form").reset();
+      },
+      function (error) {
+        alert("Erro ao se inscrever. Por favor, tente novamente.");
+        console.error("Error:", error);
+      }
+    );
+}
